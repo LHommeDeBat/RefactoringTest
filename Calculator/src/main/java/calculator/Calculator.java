0611a60b52a5a69a312530d753bfddee07212c3f
LHommeDeBot <farcry200@web.de>
2019-01-02 12:13:43 +0100

    Reordered modifier

--- Calculator/src/main/java/calculator/Calculator.java
+++ Calculator/src/main/java/calculator/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator extends SuperCalculator {
 	// Test Fork
     private String resultAsText;
 
-    public final static int MULTIPLIER = 10;
+    public static final int MULTIPLIER = 10;
 
     @Override
     public double addition(double a, double b) {
