49e382d6b7fb77c88ce23f713b53f5d0c1a782ce
LHommeDeBot <farcry200@web.de>
2019-01-21 15:18:08 +0100

    Added override annotation to method null

--- Calculator/src/main/java/calculator/Calculator.java
+++ Calculator/src/main/java/calculator/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator extends SuperCalculator {
 
     public static final int MULTIPLIER = 10;
 
+    @Override
     public double addition(double a, double b) {
         return a + b;
     }
@@ -22,5 +23,6 @@ public class Calculator extends SuperCalculator {
         return a / b;
     }
 }
+
 
 
